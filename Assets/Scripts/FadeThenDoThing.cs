a0643be658ffafae1acebbfd96e3dd2e8fcf17b3
KickTheBoot <anoojjonttu@gmail.com>
2023-04-20 21:01:05 +0300

    Make script for screen fade transitions

--- Assets/Scripts/FadeThenDoThing.cs
+++ Assets/Scripts/FadeThenDoThing.cs
@@ -6,20 +6,39 @@ using UnityEngine.UI;
 public class FadeThenDoThing : MonoBehaviour
 {
     [SerializeField]Image picture;
+    [SerializeField]Color DefaultColor, FadedColor;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    IEnumarator FadeSceneTransition()
-    {
+    public IEnumerator FadeSceneTransition(float FadeInTime, float FadeOutime)
+    {   //Fade out
+        float FastFadeInTime = 1/FadeInTime;
+        for(float t = 0; t < FadeInTime; t += Time.deltaTime)
+        {
+            picture.color = Color.Lerp(FadedColor, DefaultColor, t*FastFadeInTime);
+            yield return null;
+        }
+        Debug.Log("Did the thing!");
+        //Fade in
+        float FastFadeOutTime = 1/FadeOutime;
+        for(float t = 0; t < FadeOutime; t += Time.deltaTime)
+        {
+            picture.color = Color.Lerp(DefaultColor, FadedColor, t*FastFadeOutTime);
+            yield return null;
+        }
         yield return null;
+    }
+
+    void OnGUI()
+    {
+        if(GUILayout.Button("FadeThingy"))StartCoroutine(FadeSceneTransition(0.5f,1));
     }
 }
